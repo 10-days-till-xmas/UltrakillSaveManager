5ecd60200aeda7148b8f06acfa0eaedd6e4c2a92
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-03-22 02:51:48 +0000

    Add ConsoleUtils

---
+++ SaveManagerCLI/ConsoleUtils.cs
@@ -0,0 +1,38 @@
+﻿namespace SaveManagerCLI;
+
+public static class ConsoleUtils
+{
+    public static void Warn(object? message)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+    public static void Error(object? message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+    public static void Success(object? message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+    public static void Info(object? message)
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    public static void ClearLine(int? line = null)
+    {
+        line ??= Console.CursorTop;
+        int oldTop = Console.CursorTop;
+        Console.SetCursorPosition(0, (int)line);
+        Console.Write(new string(' ', Console.WindowWidth));
+        Console.SetCursorPosition(0, oldTop);
+    }
+}
