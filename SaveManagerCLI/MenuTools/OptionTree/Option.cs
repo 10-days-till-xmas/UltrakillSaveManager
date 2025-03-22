431e3acb3bd3ae36a9b5b15e726d9b007a4e38df
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-03-22 00:11:36 +0000

    Create Option and OptionSelector classes to be able to work with the Node-based tree system of selecting options

---
+++ SaveManagerCLI/MenuTools/OptionTree/Option.cs
@@ -0,0 +1,82 @@
+﻿namespace SaveManagerCLI.MenuTools.OptionTree;
+
+internal class Option
+{
+    private readonly struct OptionName(Option option)
+    {
+        internal readonly Option option = option;
+        public override string ToString()
+        {
+            string indentation = new(' ', (option.depth * 4) - 2);
+            string prefix;
+            if(!option.HasChildren)
+            {
+                prefix = "  ";
+            }
+            else if (option.isRoot)
+            {
+                prefix = "↓ ";
+            }
+            else
+            {
+                prefix = "→ ";
+            }
+            return indentation + prefix + option.Node.Name;
+        }
+    }
+    internal Node Node { get; init; }
+
+    private readonly OptionName optionName;
+    internal string Name => optionName.ToString();
+
+    /// <summary>
+    /// The depth of the option in the tree. 
+    /// </summary>
+    /// <remarks>
+    /// 0 is the root, 1 means 1 level deep, etc.
+    /// </remarks>
+    internal int depth;
+
+    internal readonly List<Option> Children = [];
+
+    internal Option? Parent { get; private init; }
+
+    internal bool isRoot = false;
+    internal bool HasChildren => Children.Count > 0;
+
+    internal Option(Node node, Option? Parent = null)
+    {
+        Node = node;
+        this.Parent = Parent;
+        optionName = new(this);
+
+        if (Parent is not null)
+        {
+            depth = Parent.depth + 1;
+        }
+        else
+        {
+            depth = 0;
+        }
+
+        Children = node switch
+        {
+            Branch branch => (from child in branch.children
+                              select new Option(child, this)).ToList(),
+            Leaf => [],
+            _ => throw new ArgumentException("Node must be a Branch or a Leaf"),
+        };
+    }
+
+    internal Option[] InstantiateChildren()
+    {
+        if (Node is Branch branch)
+        {
+            return branch.children.Select(child => new Option(child, this)).ToArray();
+        }
+        else
+        {
+            return [];
+        }
+    }
+}
---
+++ SaveManagerCLI/MenuTools/OptionTree/OptionSelector.cs
@@ -0,0 +1,69 @@
+﻿namespace SaveManagerCLI.MenuTools.OptionTree;
+
+internal class OptionSelector
+{
+    internal Option CurrentOption 
+    { 
+        get
+        {
+            return options[CurrentIndex];
+        }
+        private set
+        {
+            CurrentIndex = Array.IndexOf(options, value);
+        }
+    }
+
+    private int _currentIndex = 0;
+    internal int CurrentIndex
+    {
+        get => _currentIndex;
+        set
+        {
+            if (IsIndexValid(value))
+            {
+                _currentIndex = value;
+            }
+            else
+            {
+                _currentIndex = 0;
+            }
+        }
+    }
+
+    internal Option[] options;
+    internal Option? Parent => CurrentOption.Parent;
+
+    internal OptionSelector(Option[] root)
+    {
+        CurrentOption = root[0];
+        options = root;
+    }
+
+    private bool IsIndexValid(int index)
+    {
+        return 0 <= index && index < CurrentOption.Children.Count;
+    }
+
+    internal void Select(int index)
+    {
+        if (CurrentOption.Node is Leaf leaf)
+        {
+            leaf.onExecute(); // Not sure if this is a good idea
+        }
+        if (CurrentOption.HasChildren)
+        {
+            if (IsIndexValid(index))
+            {
+                CurrentOption = CurrentOption.Children[index];
+            }
+        }
+    }
+    internal void GoBack()
+    {
+        if (CurrentOption.Parent is not null)
+        {
+            CurrentOption = CurrentOption.Parent;
+        }
+    }
+}
