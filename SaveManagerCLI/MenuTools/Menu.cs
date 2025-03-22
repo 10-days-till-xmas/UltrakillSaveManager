40c0db515db8fbaa101a1e04151e27fa5166018a
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-03-22 21:00:56 +0000

    rename MainMenu to Menu and make it not a singleton

--- SaveManagerCLI/MenuTools/Menu.cs
+++ SaveManagerCLI/MenuTools/Menu.cs
@@ -2,25 +2,25 @@
 
 namespace SaveManagerCLI.MenuTools;
 
-internal class MainMenu
+internal class Menu
 {
-    private static MainMenu? _instance = null;
-    internal static MainMenu Instance
-    {
-        get
-        {
-            _instance ??= new MainMenu();
-            return _instance;
-        }
-    }
+    Branch MenuRoot { get; init; }
 
     internal void LoadMenu()
     {
         ProgramMenu programMenu = new();
+        Branch optionRoot = new("LocalOptions",
+            programMenu.SlotSaves(1),
+            programMenu.SlotSaves(2),
+            programMenu.SlotSaves(3),
+            programMenu.SlotSaves(4),
+            programMenu.SlotSaves(5)
+        );
+
         Option Root = new(programMenu.OptionsTree);
         OptionSelector selector = new(Root);
         Console.Clear();
-        Console.WriteLine("Select one of the following Options to proceed:");
+        Console.WriteLine("Select one of the following LocalOptions to proceed:");
         selector.PrintOptionSelector();
         // TODO: Implement the rest of the program. Handle executing the leaves
     }
