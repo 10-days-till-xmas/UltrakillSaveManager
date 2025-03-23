86b84007e312c44c909158a6869ecf024ee93254
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-03-23 13:08:42 +0000

    Implement SimpleDirectoryExplorer

---
+++ SaveManagerCLI/SaveManipulation/SimpleDirectoryExplorer.cs
@@ -0,0 +1,71 @@
+﻿using SaveManagerCLI.OptionTree;
+
+namespace SaveManagerCLI.SaveManipulation;
+
+internal class SimpleDirectoryExplorer
+{
+    internal enum DirectorySorting
+    {
+        None = 0,
+        Alphabetical,
+        Type
+    }
+
+    private readonly Branch<DirectoryInfo> rootBranch;
+    public SimpleDirectoryExplorer(string rootDir)
+    {
+        DirectoryInfo directory = new(rootDir);
+        Node[] children = CreateNodeTree(directory);
+        rootBranch = new Branch<DirectoryInfo>(directory.Name, directory, children);
+    }
+
+    public static Node[] CreateNodeTree(DirectoryInfo directory)
+    {
+        List<Node> nodes = [];
+        foreach (var file in directory.GetFiles())
+        {
+            nodes.Add(new Leaf<FileInfo>(file.Name, file));
+        }
+        foreach (var subDir in directory.GetDirectories())
+        {
+            Node[] dirChildren = CreateNodeTree(subDir);
+            nodes.Add(new Branch<DirectoryInfo>(subDir.Name, subDir, dirChildren));
+        }
+        return nodes.ToArray();
+    }
+
+    public static void PrintDirectoryTree()
+    {
+        SimpleDirectoryExplorer explorer = new(ProgramSettings.SaveDirectory);
+        Option root = new(explorer.rootBranch);
+        OptionSelector selector = new(root);
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Gray; 
+        Console.WriteLine(root.Name);
+        Console.ResetColor();
+        FileInfo selectedSave;
+        Console.CursorTop = 0;
+        do
+        {
+            selectedSave = selector.PrintOptionSelector<FileInfo>(true, true);
+            if (selectedSave is null)
+            {
+                return;
+            }
+            if (selectedSave.Extension is ".bepis")
+            {
+                break;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            ConsoleUtils.ClearLine();
+            Console.WriteLine("Invalid File Type");
+            --Console.CursorTop;
+            Console.ResetColor();
+        } while (true);
+        
+        
+        SaveNavigator navigator = new(selectedSave);
+        navigator.PrintSaveData();
+        Console.ReadKey(true);
+    }
+}
