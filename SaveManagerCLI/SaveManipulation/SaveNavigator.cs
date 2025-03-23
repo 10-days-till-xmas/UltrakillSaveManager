b9eddbc68e52ea85e5d9dbb714afb26f245fc078
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-03-23 13:12:03 +0000

    Add SaveNavigator.cs

---
+++ SaveManagerCLI/SaveManipulation/SaveNavigator.cs
@@ -0,0 +1,31 @@
+﻿using UltrakillSaveManager.SaveFile;
+
+namespace SaveManagerCLI.SaveManipulation;
+
+public class SaveNavigator
+{
+    private readonly FileInfo saveFileInfo;
+    private readonly TypedSaveFile saveFile;
+    public SaveNavigator(FileInfo saveFile)
+    {
+        saveFileInfo = saveFile;
+        this.saveFile = LoadSave();
+    }
+
+    internal TypedSaveFile LoadSave()
+    {
+        return TypedSaveFile.GetSaveFile(saveFileInfo.FullName);
+    }
+
+    public void PrintSaveData()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine(saveFileInfo.Name);
+        Console.ResetColor();
+        foreach (var field in saveFile.GetFields())
+        {
+            Console.WriteLine($"{field.Name}: {field.GetValue(saveFile.SaveData)}");
+        }
+    }
+}
