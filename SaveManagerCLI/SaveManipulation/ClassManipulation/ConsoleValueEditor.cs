7a5e3e749df7dad54909b7ad5dc82e12eece8623
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-04-04 09:32:26 +0100

    Add a class to enable editing a Wrapper

---
+++ SaveManagerCLI/SaveManipulation/ClassManipulation/ConsoleValueEditor.cs
@@ -0,0 +1,58 @@
+﻿namespace SaveManagerCLI.SaveManipulation.ClassManipulation;
+
+internal class ConsoleValueEditor
+{
+    internal Wrapper ValueWrapper { get; init; }
+    internal string ValueName { get; init; }
+    internal Type ValueType => ValueWrapper.WrappedType;
+
+    internal string ValueTypeName => ValueType.Name;
+
+    internal ConsoleValueEditor(string name, Wrapper wrapper)
+    {
+        ValueName = name;
+        ValueWrapper = wrapper;
+    }
+
+    public void PrintValueEditor()
+    {
+        Console.WriteLine($"Editing {ValueName}");
+        Console.WriteLine($"Type: {ValueTypeName}");
+        Console.WriteLine($"Current value: {ValueWrapper.Getter()}");
+        Console.WriteLine("Enter new value:");
+        bool repeat = true;
+        do
+        {
+            string? newValue = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(newValue))
+            {
+                Console.WriteLine("Invalid input");
+                continue;
+            }
+            if (TryParseValue(newValue, out object? result))
+            {
+                ValueWrapper.Setter(result);
+                Console.WriteLine("Value set");
+                repeat = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        } while (repeat);
+    }
+
+    private bool TryParseValue(string value, out object? result)
+    {
+        try
+        {
+            result = Convert.ChangeType(value, ValueType);
+            return true;
+        }
+        catch (Exception)
+        {
+            result = null;
+            return false;
+        }
+    }
+}
