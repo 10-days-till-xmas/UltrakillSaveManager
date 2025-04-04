6777ad897f9691e6504b1bb8c8fc43d2930ae461
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-04-04 09:36:23 +0100

    Add ProgramTest, used when debugging OptionSelector

--- SaveManagerCLI/Program.cs
+++ SaveManagerCLI/Program.cs
@@ -1,10 +1,18 @@
 ﻿using SaveManagerCLI;
 using SaveManagerCLI.OptionTree;
 using SaveManagerCLI.SaveManipulation;
+using SaveManagerCLI.SaveManipulation.ClassManipulation;
 using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
 Console.Title = "Save Manager CLI";
+
+SaveNavigator navigator = new(new FileInfo(@"C:\Program Files (x86)\Steam\steamapps\common\ULTRAKILL\Saves\Slot1\generalprogress.bepis"));
+
+ClassViewer viewer = new("difficulty0progress", navigator.saveFile.SaveData, navigator.saveFile.Type);
+viewer.PrintOptions();
+
+ConsoleUtils.WaitForEnterPress();
 
 Branch MainMenu = new("Main SaveMenu",
     new Leaf<Action>("Modify Saves", SimpleDirectoryExplorer.PrintDirectoryTree),
@@ -12,6 +20,7 @@ Branch MainMenu = new("Main SaveMenu",
     new Leaf<Action>("Exit", () => Environment.Exit(0))
     );
 OptionSelector baseSelector = new(new Option(MainMenu));
+
 while (true)
 {
     Console.Clear();
---
+++ SaveManagerCLI/ProgramTest.cs
@@ -0,0 +1,50 @@
+﻿using SaveManagerCLI.OptionTree;
+
+namespace SaveManagerCLI;
+
+internal static class ProgramTest
+{
+    internal static Leaf<Action>[] MakeLeaves(string namePrefix, int count, int start)
+    {
+        Leaf<Action>[] leaves = new Leaf<Action>[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            leaves[i] = new Leaf<Action>($"{namePrefix}{start + i}", () => Console.WriteLine($"{namePrefix} {start + i}"));
+        }
+        return leaves;
+    }
+
+    internal static Branch MakeBranchOfLeaves(string namePrefix, int count)
+    {
+        return new Branch(namePrefix, MakeLeaves(namePrefix, count, 0));
+    }
+
+    // Over the top, but it covers many cases
+    internal static readonly Branch TestBranch =
+        new("Main Root Test", [
+            ..MakeLeaves("L", 20, 0),
+            new Branch("B20", [
+                ..MakeLeaves("B21.L", 15, 0),
+                new Branch("B21.B15", [
+                    ..MakeLeaves("B21.B15.L", 15, 0),
+                    new Branch("B21.B15.B15", [
+                        ..MakeLeaves("B21.B15.B15.L", 15, 0),
+                        MakeBranchOfLeaves("B21.B15.B15.B15", 15),
+                        ..MakeLeaves("B21.B15.B15.L", 15, 0),
+                        ])
+                    ])
+                ]),
+            ..MakeLeaves("L", 15, 21)
+            ]);
+
+    internal static readonly OptionSelector testSelector = new(new Option(TestBranch));
+
+    internal static void Test()
+    {
+        Console.Clear();
+        //Action onExecute = baseSelector.PrintOptionSelector<Action>();
+        Action onExecute = testSelector.PrintOptionSelector<Action>();
+        onExecute();
+    }
+}
