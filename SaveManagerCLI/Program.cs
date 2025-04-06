e2a828db655d93b72c199329ffd0790ca962ccf0
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-04-06 03:45:05 +0100

    Refactor Program.cs, and prepare for the initial prerelease

--- SaveManagerCLI/Program.cs
+++ SaveManagerCLI/Program.cs
@@ -1,29 +1,31 @@
-﻿using SaveManagerCLI;
-using SaveManagerCLI.OptionTree;
+﻿using SaveManagerCLI.OptionTree;
 using SaveManagerCLI.SaveManipulation;
-using SaveManagerCLI.SaveManipulation.ClassManipulation;
 using System.Text;
 
-Console.OutputEncoding = Encoding.UTF8;
-Console.Title = "Save Manager CLI";
-
-SaveNavigator navigator = new(new FileInfo(@"C:\Program Files (x86)\Steam\steamapps\common\ULTRAKILL\Saves\Slot1\generalprogress.bepis"));
-
-ClassViewer viewer = new("difficulty0progress", navigator.saveFile.SaveData, navigator.saveFile.Type);
-viewer.PrintOptions();
-
-ConsoleUtils.WaitForEnterPress();
-
-Branch MainMenu = new("Main SaveMenu",
-    new Leaf<Action>("Modify Saves", SimpleDirectoryExplorer.PrintDirectoryTree),
-    ProgramSettings.Branch,
-    new Leaf<Action>("Exit", () => Environment.Exit(0))
-    );
-OptionSelector baseSelector = new(new Option(MainMenu));
-
-while (true)
+namespace SaveManagerCLI;
+internal class Program
 {
-    Console.Clear();
-    Action onExecute = baseSelector.PrintOptionSelector<Action>();
-    onExecute();
+    internal readonly static Branch MainMenu = new("Main SaveMenu",
+                                                   new Leaf<Action>("Modify Saves", SimpleDirectoryExplorer.PrintDirectoryTree),
+                                                   ProgramSettings.Branch,
+                                                   new Leaf<Action>("Exit", () => Environment.Exit(0)));
+
+    internal readonly static OptionSelector mainMenuSelector = new(new Option(MainMenu));
+    private static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Title = "Save Manager CLI";
+
+        MenuLoop();
+    }
+
+    private static void MenuLoop()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Action onExecute = mainMenuSelector.PrintOptionSelector<Action>();
+            onExecute();
+        }
+    }
 }
